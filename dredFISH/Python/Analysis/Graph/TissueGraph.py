a5bcd950b2592b2b96ba9d1e31e44a8e34e3b593
Roy Wollman <rwollman@ucla.edu>
2021-11-18 21:50:08 -0800

    TissueGraph almost ready to general use

--- dredFISH/Python/Analysis/Graph/TissueGraph.py
+++ dredFISH/Python/Analysis/Graph/TissueGraph.py
@@ -3,25 +3,39 @@
 A collection of files to analyze tissues using Graph representation
 
 This file should be imported as a module and contains the following
-functions:
+main functions:
 
     * BuildSpatialGraph - construct graph based on Delauny neighbors
-    * AddTypeInfo - adds a type attribute to all vertices (cells / zones / environments)
+    * ContractGraph - find zones, i.e. spatially continous areas in the graph the same type
+    * CondEntopy - calculates the conditional entropy of a graph given types (or uses defaults existing types)
+    
+Many other accessory functions that are used to set/get TissueGraph information. 
     
 """
 
 # dependeices
 from igraph import *
 from scipy.spatial import Delaunay,Voronoi
+from collections import Counter
 import numpy as np
 import warnings
 
 class TissueGraph: 
     def __init__(self): 
-        self._G=None
-        self.N = None
+        self._G = None
+        self.UpstreamMap = None
         return None
         
+    @property
+    def N(self):
+        """Size of the tissue graph
+            internally stored as igraph size
+        """
+        if self._G is not None:
+            return len(self._G.vs)
+        else: 
+            raise ValueError('Graph was not initalized, please build a graph first with BuildSpatialGraph')
+    
     @property
     def G(self):
         if self._G is not None:
@@ -40,7 +54,6 @@ class TissueGraph:
             G - an igraph 
 
         """
-        
         # validate input
         if not isinstance(XY, np.ndarray):
             raise ValueError('XY must be a numpy array')
@@ -48,14 +61,11 @@ class TissueGraph:
         if not XY.shape[1]==2:
             raise ValueError('XY must have only two columns')
             
-        # update number of items
-        self.N = XY.shape[0]
-        
         # start with triangulation
         dd=Delaunay(XY)
 
         # create adjacency matrix
-        AdjMat=np.zeros([self.N,self.N]).astype(np.bool_)
+        AdjMat=np.zeros([XY.shape[0],XY.shape[0]]).astype(np.bool_)
         for x in dd.simplices:
             AdjMat[x[0],x[1]]=True
             AdjMat[x[1],x[0]]=True
@@ -77,7 +87,7 @@ class TissueGraph:
         """
             XY : dependent property - will query info from Graph and return
         """
-        XY=np.zeros(self.N,2)
+        XY=np.zeros((self.N,2))
         XY[:,0]=self._G.vs["X"]
         XY[:,1]=self._G.vs["Y"]
         return(XY)
@@ -102,7 +112,7 @@ class TissueGraph:
                 warns when None. 
         """
         # validate that Type attribute was initialized 
-        if "Type" not in G.vs.attribute_names():
+        if "Type" not in self._G.vs.attribute_names():
             warnings.warn("Type was not initalized - returning None")
             return(None)
         
@@ -112,7 +122,10 @@ class TissueGraph:
     
     @Type.setter
     def Type(self, TypeVec):
-        
+        """
+            Assign Type values, stores them using igraph attributes. 
+        """
+        # stores Types as graph vertex attributes. 
         self._G.vs["Type"]=TypeVec
         return
   
@@ -126,26 +139,77 @@ class TissueGraph:
         
         Output: a new TissueGraph after vertices merging. 
         """
-         # get edge list    
-        EL = np.asarray(G.get_edgelist()).astype("int")
-        nm=G.vs["name"]
+         # get edge list - work with names and not indexes in case things shift around (they shouldn't),     
+        EL = np.asarray(self._G.get_edgelist()).astype("int")
+        nm=self._G.vs["name"]
         EL[:,0] = np.take(nm,EL[:,0])
         EL[:,1] = np.take(nm,EL[:,1])
 
-        SameTypeIX = self.Type[SpatialNeighberhood[,1]] == self.Type[SpatialNeighberhood[,2]]
+        # only keep edges where neighbors are of same types
+        EL = EL[np.take(self.Type,EL[:,0]) == np.take(self.Type,EL[:,1]),:]
         
+        # remake a graph with potentially many components
+        IsoZonesGraph = Graph(n=self.N, edges=EL)
+        IsoZonesGraph = IsoZonesGraph.as_undirected().simplify()
+        IsoZonesGraph.vs["name"] = IsoZonesGraph.vs.indices
+
+        # because we used both type and proximity, the original graph (based only on proximity)
+        # that was a single component graph will be broken down to multiple components 
+        # decompose is a method from igraph that returns a list of all individual components (i.e. zones) 
+        DecomposedZones = Graph.decompose(IsoZonesGraph) 
         
-    
+        IxMapping = np.empty(self.N)
+        ZoneTypes = []
+        ZoneXY=np.empty((len(DecomposedZones),2))
+        
+        for i in range(len(DecomposedZones)):
+            ix=DecomposedZones[i].vs.get_attribute_values("name")
+            IxMapping[ix]=i
+            ZoneTypes.append(self.Type[ix[0]])
+            ZoneXY[i,:]=np.mean(self.XY[ix,:],axis=0)
+
+        
+        ZoneGraph = TissueGraph()
+        ZoneGraph.BuildSpatialGraph(ZoneXY)
+        ZoneGraph.Type = ZoneTypes
+        ZoneGraph._G.vs["Size"] = [x.vcount() for x in DecomposedZones]
+        ZoneGraph.UpstreamMap = IxMapping
+        
+        return(ZoneGraph)
+                             
+    @property    
+    def Ntypes(self): 
+        """ 
+            Ntypes: returns number of unique types in the graph
+        """ 
+        if self.Type == None: 
+            raise ValueError("Type not yet assigned, can't count how many")
+        return(len(numpy.unique(self.Type)))
+                             
+    def TypeFreq(self): 
+        """
+            TypeFreq: return the catogorical probability for each type
+        """
+        if self.Type == None: 
+            raise ValueError("Type not yet assigned, can't count frequencies")
+        Ptypes=np.array(list(Counter(self.Type).values()))
+        Ptypes=Ptypes/np.sum(Ptypes)
+        return(Ptypes)
+                             
     def CondEntropy(self):
         """
         CondEntropy: calculate conditional entropy of the tissue graph
                      cond entropy is the difference between graph entropy based on pagerank and type entropy
         """
+
         Pzones = self._G.pagerank()
         Entropy_Zone = -np.sum(Pzones*np.log2(Pzones))
         
-        Ptypes=np.array(list(Counter(self.Type).values()))
-        Ptypes=Ptypes/np.sum(Ptypes)
+        # validate that type exists
+        if self.Type == None: 
+            raise ValueError("Cann't calculate cond-entropy without Types, please check")
+            
+        Ptypes = self.TypeFreq(); 
         Entropy_Types=-np.sum(Ptypes*np.log2(Ptypes))
         
         CondEntropy = Entropy_Zone-Entropy_Types
