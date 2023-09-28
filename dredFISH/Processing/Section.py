3325bc8485aafc4feca112ff89f1921005ec2a3d
zehemminger <zehemminger@g.ucla.edu>
2023-09-28 11:32:50 -0700

    Update Section.py

--- dredFISH/Processing/Section.py
+++ dredFISH/Processing/Section.py
@@ -823,8 +823,8 @@ class Section_Class(object):
             self.data = self.data[:,self.data.var.index.isin(['PolyT','Nonspecific_Encoding','Nonspecific_Readout'])==False]
 
             self.save(self.data.obs,type='metadata',model_type=model_type)
-            self.save(pd.DataFrame(self.data.X,index=self.data.obs.index,columns=self.data.var.index),type='matrix',model_type=model_type)
-            self.save(pd.DataFrame(self.data.X,index=self.data.obs.index,columns=self.data.var.index),type='matrix_raw',model_type=model_type)
+            self.save(pd.DataFrame(self.data.layers['processed_vectors'],index=self.data.obs.index,columns=self.data.var.index),type='matrix',model_type=model_type)
+            self.save(pd.DataFrame(self.data.layers['raw_vectors'],index=self.data.obs.index,columns=self.data.var.index),type='matrix_raw',model_type=model_type)
             self.save(self.data,type='anndata',model_type=model_type)
 
 
