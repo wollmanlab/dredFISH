55bb3d60a925979bf00f27def56ce9b31cc58e93
zehemminger <zehemminger@g.ucla.edu>
2024-03-06 14:32:10 -0800

    path to new allen data release

--- dredFISH/Utils/pathu.py
+++ dredFISH/Utils/pathu.py
@@ -4,7 +4,7 @@ datasets = {
     'allen_smrt_dpnmf': '/greendata/GeneralStorage/fangming/projects/dredfish/data/rna/scrna_ss_ctxhippo_a_exon_DPNMF_matrix.h5ad',
     'allen_tenx_dpnmf': '/greendata/GeneralStorage/fangming/projects/dredfish/data/rna/scrna_10x_ctxhippo_a_DPNMF_matrix.h5ad',
     'allen_anat_tree': '/greendata/GeneralStorage/fangming/reference/allen_ccf/structures.json',
-    'allen_wmb_tree': '/orangedata/ExternalData/Allen_WMB_2023Sep05/projected_Tree_reorder_2023Sep05/10X_combined.h5ad',
+    'allen_wmb_tree': '/orangedata/ExternalData/Allen_WMB_2023Sep05/projected_Tree_reorder_2024Mar06/10X_combined.h5ad',
     'allen_smrt_tree':'/greendata/binfo/mouse/Brain/Sequencing/Allen_SmartSeq_2020/projected_tree_2023Sep08.h5ad',
 }
 
