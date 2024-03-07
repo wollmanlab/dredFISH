68b1cf9c7b1d7770c8d60bdcc4b4445d5e2cc0eb
zehemminger <zehemminger@g.ucla.edu>
2024-03-06 17:16:02 -0800

    add spatial reference path

--- dredFISH/Utils/pathu.py
+++ dredFISH/Utils/pathu.py
@@ -6,6 +6,7 @@ datasets = {
     'allen_anat_tree': '/greendata/GeneralStorage/fangming/reference/allen_ccf/structures.json',
     'allen_wmb_tree': '/orangedata/ExternalData/Allen_WMB_2023Sep05/projected_Tree_reorder_2024Mar06/10X_combined.h5ad',
     'allen_smrt_tree':'/greendata/binfo/mouse/Brain/Sequencing/Allen_SmartSeq_2020/projected_tree_2023Sep08.h5ad',
+    'spatial_reference':'/orangedata/ExternalData/Allen_WMB_2024Mar06/spatial_data.h5ad'
 }
 
 def get_path(key, datasets=datasets, check=False):
