4029dd17357b6336299e204b75c47e3a50e65968
zehemminger <zehemminger@g.ucla.edu>
2024-06-25 08:52:47 -0700

    executable

---
+++ dredFISH/Analysis/execute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import argparse
+from dredFISH.Utils.analysisu import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("animal", type=str, help="Name of the animal to analyze")
+    parser.add_argument("-p", "--project_path", type=str, dest="project_path", default='/scratchdata1/Images2024/Zach/MouseBrainAtlas', action='store', help="Path to the project directory (default: '/scratchdata1/Images2024/Zach/MouseBrainAtlas')")
+    parser.add_argument("-a", "--analysis_path", type=str, dest="analysis_path", default='/scratchdata1/MouseBrainAtlases_V1', action='store', help="Path to the analysis directory (default: '/scratchdata1/MouseBrainAtlases_V0')")
+
+    args = parser.parse_args()
+    if not os.path.exists(args.analysis_path):
+        os.mkdir(args.analysis_path,mode=0o777)
+
+    analyze_mouse_brain_data(args.animal,
+                            project_path=args.project_path,
+                            analysis_path=args.analysis_path,
+                            verbose=False)
